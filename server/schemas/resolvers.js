93bba7c0701c7046e0127e5b62bda82f8c7ea8bc
Calcifer <shanna.renee.morris@gmail.com>
2023-08-25 16:46:13 -0700

    added comments

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,7 @@
 const { category, gen_merch, lifeForm, payment_type, real_estate, travel } = require('../models');
 
+
+// I don't really know what we are doing with these yet so we need to play around these are tentative
 const resolvers = {
   Query: {
     lifeForm: async () => {
