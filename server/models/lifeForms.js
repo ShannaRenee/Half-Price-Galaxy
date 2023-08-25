e0d1a8008c553a590beb59987b43e5a5e46d736a
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-24 20:24:01 -0700

    Start build

---
+++ server/models/lifeForms.js
@@ -0,0 +1,62 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// import schema from Book.js
+const bookSchema = require('./Book');
+
+const lifeFormSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+      maxLength: 30,
+    },
+    last_name: {
+      type: String,
+      required: true,
+      maxLength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // set savedBooks to be an array of data that adheres to the bookSchema
+    savedBooks: [bookSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+lifeFormSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+lifeFormnSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+userSchema.virtual('bookCount').get(function () {
+  return this.savedBooks.length;
+});
+
+const lifeForm = model('lifeForm', lifeFormSchema);
+
+module.exports = lifeForm;
