9f76896ec7f241a1af610f960deead9ea705ce73
Ismael Jimenez Jurado <ismael@eduphoria.net>
2023-08-24 20:18:09 -0600

    changing file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
+import Matchup from './pages/Matchup';
+import Vote from './pages/Vote';
+import NotFound from './pages/NotFound';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />}
+            />
+            <Route 
+              path="/matchup" 
+              element={<Matchup />}
+            />
+            <Route 
+              path="/matchup/:id" 
+              element={<Vote />}
+            />
+            <Route 
+              path="*"
+              element={<NotFound />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
