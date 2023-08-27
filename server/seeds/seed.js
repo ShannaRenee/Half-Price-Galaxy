2e654c17f4109c00399f2ff429afc8168e5d865b
Larissa Stack <larissastack@gmail.com>
2023-08-26 22:14:28 -0700

    seeds finished.

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,14 +1,23 @@
 const db = require('../config/connection');
-const { User, Realestate } = require('../models'); //Links to User model
+const { Lifeforms, Realestate, GenMerch, Travel, Categories, Payment } = require('../models'); //Links to User model
 
-const userData = require('./userData.json'); //Links to userData.json
+const lifeformData = require('./lifeformData.json'); //Links to userData.json
 const realestateData = require('./realestateData.json'); //Links to realestateData.json
+const genMerchData = require('./genMerchData.json'); //Links to genMercData.json
+const travelData = require('./travelData.json'); //Links to travelData.json
+const categoriesData = require('./categoriesData.json'); //Links to categoriesData.json
+const paymentData = require('./paymentData.json'); //Links to paymentData.json
 
 db.once('open', async () => {
   await User.deleteMany({});
 
-  const users = await User.insertMany(userData);
+  const Lifeforms = await User.insertMany(userData);
+  const Realestate = await User.insertMany(realestateData);
+  const GenMerch = await User.insertMany(genMerchData);
+  const Travel = await User.insertMany(travelData);
+  const Categories = await User.insertMany(categoriesData);
+  const Payment = await User.insertMany(paymentData);
 
-  console.log('Users seeded!');
+  console.log('Data seeded!');
   process.exit(0);
 });
