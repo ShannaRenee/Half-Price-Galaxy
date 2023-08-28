d380424595c60d26d5166e2d9c1cd6865c5e50e5
Calcifer <shanna.renee.morris@gmail.com>
2023-08-28 14:29:13 -0700

    deleted exclamation points

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,46 +4,46 @@ const { gql } = require('apollo-server-express');
 // I think this is the general idea but feel free to tweak as needed
 const typeDefs = gql`
   type lifeForm {
-    _id: ID!
-    first_name: String!
-    last_name: String!
-    email: String!
-    password: String!
-    home_planet: String!
+    _id: ID
+    first_name: String
+    last_name: String
+    email: String
+    password: String
+    home_planet: String
   }
 
   type categories {
-    _id: ID!
-    name: String!
+    _id: ID
+    name: String
   }
 
   type gen_merch {
-    _id: ID!
-    item_name: String!
-    description: String!
-    price: String!
-    categories: [categories]
+    _id: ID
+    item_name: String
+    description: String
+    price: String
+    category: [categories]
   }
 
   type real_estate {
-    _id: ID!
-    item_name: String!
-    item_description: String!
-    item_price: String!
-    categories: [categories]
+    _id: ID
+    item_name: String
+    item_description: String
+    item_price: String
+    category: [categories]
   }
 
   type travel {
-    _id: ID!
-    package_name: String!
-    description: String!
-    price: String!
-    categories: [categories]
+    _id: ID
+    package_name: String
+    description: String
+    price: String
+    category: [categories]
   }
 
   type payment_type {
-    _id: ID!
-    name: String!
+    _id: ID
+    name: String
     lifeForm: [lifeForm]
   }
 
