b59e1b499eaeed159d1e6fad31a7361996220f93
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-28 19:54:55 -0700

    change home and added assets folder

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,14 +10,14 @@ const Home = () => {
   const matchupList = data?.matchups || [];
 
   return (
-    <div className="card bg-white card-rounded w-50">
+    <div className="card bg-white card-rounded w-75">
       <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
+        <h1>👽 Welcome to Half Price Galaxy   </h1><button className='btn btn-small btn-danger'>Login</button>
       </div>
       <div className="card-body m-5">
-        <h2>Here is a list of matchups you can vote on:</h2>
+        <h2>  </h2>
         {loading ? (
-          <div>Loading...</div>
+          <div>Thrusters warming....</div>
         ) : (
           <ul className="square">
             {matchupList.map((matchup) => {
@@ -33,9 +33,21 @@ const Home = () => {
         )}
       </div>
       <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
+        <h2>Take a gander at some prime interstellar realestate.</h2>
+        <Link to="/realestate">
+          <button className="btn btn-lg btn-danger">Ready to start your adventure?</button>
+        </Link>
+      </div>
+      <div className="card-footer text-center m-3">
+        <h2>Want some stuff look no further!</h2>
+        <Link to="/realestate">
+          <button className="btn btn-lg btn-danger">Ready to start your adventure?</button>
+        </Link>
+      </div>
+      <div className="card-footer text-center m-3">
+        <h2>Want to get out off your dusty old planet? Click me!</h2>
+        <Link to="/realestate">
+          <button className="btn btn-lg btn-danger">Ready to start your adventure?</button>
         </Link>
       </div>
     </div>
