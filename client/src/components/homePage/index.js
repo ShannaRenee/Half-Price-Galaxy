cabc599339038c7e758bdb1d0616db2cbb1137df
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-29 21:04:15 -0700

    homepage

---
+++ client/src/components/homePage/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./style.css";
+
+
+const HomePage = ({homePages}) => {
+    console.log(homePages);
+    return (
+        <div class="card">
+            <img 
+            src={require(`../../../assets/imgs/${homePages.name}.jpg`)} 
+            class="card-img-top" 
+            alt={homePages.name}
+            />
+            <div class="card-body">
+                <h5 class="card-title">{homePages.name}</h5>
+                <p class="card-text">{homePages.description}</p>
+            </div>
+           
+        </div>
+            );
+};
+
+export default HomePage;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import { homePages } from '../data';
-import Homes from '../components/homes';
-
+import HomePage from '../components/homePage/';
 
 const Home = () => {
+  console.log(homePages);
   return (
     <div className="card bg-white card-rounded w-75">
       <div className="card-header bg-dark text-center">
         <h1>👽 Welcome to Half Price Galaxy </h1>
         <button className="btn btn-small btn-danger">Login</button>
       </div>
-      
-      <div className="card-footer text-center m-3">
-        <h2>Finally ready to your dream home?</h2>
-        {homePages.map((homePages) => (
-                <Homes homePages={homePages} key={i}/>
-            ))}
+      <div className="card-body">
+        {homePages.map((homePage, i) => (
+          <HomePage key={homePage.name} homePage={homePage} />
+        ))}
       </div>
-
-      <div className="card-footer text-center m-3">
-        <h2>Want some stuff look no further!</h2>
-        {/* Rest of the component content */}
-      </div>
-
-      <div className="card-footer text-center m-3">
-        <h2>Want to get out off your dusty old planet? Click me!</h2>
-        {/* Rest of the component content */}
-      </div>
-
     </div>
   );
 };
