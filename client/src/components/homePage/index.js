9113a23513578902ac3ac0a07b87a4163a681a29
Calcifer <shanna.renee.morris@gmail.com>
2023-09-04 22:46:10 -0700

    fixed file path

--- client/src/components/homePage/index.js
+++ client/src/components/homePage/index.js
@@ -3,15 +3,15 @@ import "./style.css";
 const HomePage = ({homePages}) => {
     console.log(homePages);
     return (
-        <div class="card bg-dark">
+        <div className="card bg-dark">
             <img
-            src={require(`../../../assets/imgs/${homePages.name}.jpg`)}
-            class="card-img-top"
+            src={require(`../../assets/imgs/${homePages.name}.jpg`)}
+            className="card-img-top"
             alt={homePages.name}
             />
-            <div class="card-body bg-danger w-100">
-                <h5 class="card-title">{homePages.name}</h5>
-                <p class="card-text">{homePages.description}</p>
+            <div className="card-body bg-danger w-100">
+                <h5 className="card-title">{homePages.name}</h5>
+                <p className="card-text">{homePages.description}</p>
             </div>
         </div>
             );
